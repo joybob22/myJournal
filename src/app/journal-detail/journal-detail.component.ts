3c2b8567f35e67b365abf11dcef39fe2284f6ea2
joybob22 <braydenlemke8@gmail.com>
2020-07-13 19:25:32 -0600

    created an edit mode for the tags

--- src/app/journal-detail/journal-detail.component.ts
+++ src/app/journal-detail/journal-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { JournalService } from '../services/journal.service';
 import { Journal } from '../models/journal.model';
 import { TagsService } from '../services/tags.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-journal-detail',
@@ -13,18 +15,66 @@ export class JournalDetailComponent implements OnInit {
 
   journalId:string;
   journal:Journal;
-  tags: Array<string>
+  editMode:boolean = false;
+  tags: Array<string>;
+  newTagForm: FormGroup;
+  newTagName: string;
+  selectedTags: Array<string> = [];
+
   constructor(
     private _route: ActivatedRoute,
     private journalService: JournalService,
-    private tagsService: TagsService
+    private tagsService: TagsService,
+    private fb: FormBuilder,
+    private renderer: Renderer2
   ) { }
 
   ngOnInit(): void {
     this.journalId = this._route.snapshot.params['id'];
     this.journal = this.journalService.getJournalById(this.journalId);
-    this.tags = this.tagsService.tags;
-    console.log(this.journal);
+    this.tags = this.tagsService.getTags;
+    this.newTagForm = this.fb.group({
+      tagName: ['', Validators.required]
+    });
+  }
+
+  handleEditClick() {
+    this.editMode = !this.editMode;
+  }
+
+  handleNewTagEnterKey(event) {
+    if(event.key === "Enter") {
+      this.createNewTag();
+    }
+  }
+
+  createNewTag() {
+    this.newTagForm.markAllAsTouched();
+    if(this.newTagForm.status === "VALID") {
+      this.tagsService.createNewTag(this.newTagForm.value.tagName);
+      this.newTagName = '';
+      this.newTagForm.reset();
+    }
+  }
+
+  handleTagClick(tag, event) {
+    if(this.editMode) {
+      this.tagsService.removeTag(tag);
+      this.tags = this.tagsService.getTags;
+    } else {
+      const hasClass = event.target.classList.contains("activeTag");
+      if(hasClass) {
+        this.renderer.removeClass(event.target, "activeTag");
+        this.selectedTags = this.selectedTags.filter(item => {
+          if(item !== tag) {
+            return true;
+          } 
+        });
+      } else {
+        this.selectedTags.push(tag);
+        this.renderer.addClass(event.target, "activeTag");
+      }
+    }
   }
 
 
--- src/app/services/tags.service.ts
+++ src/app/services/tags.service.ts
@@ -5,9 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class TagsService {
 
+  tags: Array<string> = ['Gratitude', 'Love', 'Spiritual', 'Hard Days', 'The Grind', 'Service', 'Work'];
+
   constructor() { }
 
-  get tags() {
-    return ['Gratitude', 'Love', 'Spiritual', 'Hard Days', 'The Grind', 'Service', 'Work']
+  get getTags() {
+    return this.tags
   }
+
+  createNewTag(tag:string):void {
+    this.tags.push(tag);
+  }
+
+  removeTag(tag:string):void {
+    this.tags = this.tags.filter(item => {
+      if(item !== tag) {
+        return true;
+      }
+    })
+  }
+ 
 }
